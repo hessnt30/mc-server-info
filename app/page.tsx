278e265da038884b1c7af32d5695b35e04e75cf5
hessnt30 <hessnt30@vt.edu>
2025-01-07 11:00:53 -0500

    fixed responsiveness for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,9 @@ import SearchServer from "@/components/search-server";
 export default function Home() {
   return (
     <main
-      className="flex-1 flex flex-col gap-6 px-4 mx-auto max-w-lg"
-      style={{ width: "800px" }}
+      className="flex flex-col gap-6 px-4 mx-auto max-w-lg w-full"
+      style={{ maxWidth: "800px" }}
     >
-      <h2 className="font-medium text-xl text-center">
-        Check MC Server Status
-      </h2>
       <SearchServer isAuthenticated={false} />
     </main>
   );
